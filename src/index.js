b1a370c25d506676b66ce950086d69d4470fadcf
Evgen1983 <ebaranov1983@gmail.com>
2017-07-06 19:28:40 +0300

    add eslint

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { AppContainer } from 'react-hot-loader';
+
+import App from './App';
+
+import '../semantic/dist/semantic.min.css';
+
+const rootEl = document.getElementById('app');
+
+ReactDOM.render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  rootEl,
+  () => {
+    delete window.__INITIAL_STATE__;
+  }
+);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp />
+      </AppContainer>,
+      rootEl
+    );
+  });
+}
