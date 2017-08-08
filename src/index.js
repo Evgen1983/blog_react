08473425be800ab28ad18a6eaa8b9402a9740d2f
Evgen1983 <ebaranov1983@gmail.com>
2017-08-08 17:55:10 +0300

    change c3 route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from 'App';
-import '../node_modules/c3/c3.min.css';
+import 'c3/c3.min.css';
 import '../bootstrap/css/bootstrap.min.css';
 import './css/style.css';
 
